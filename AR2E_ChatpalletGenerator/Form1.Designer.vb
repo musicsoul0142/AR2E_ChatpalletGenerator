9742068b6f550c6cb9505267cf2fd4a9093c53d4
musicsoul0142 <mekakushi0142@gmail.com>
2023-03-05 23:37:59 +0900

    SL1以外のスキルレベルを編集としてチャパレへ出力できるように

--- AR2E_ChatpalletGenerator/Form1.Designer.vb
+++ AR2E_ChatpalletGenerator/Form1.Designer.vb
@@ -22,71 +22,79 @@ Partial Class MainWindow
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
-        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.SuspendLayout()
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(25, 94)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(76, 48)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "開く"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'DataGridView1
-        '
-        Me.DataGridView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(232, 12)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.RowTemplate.Height = 25
-        Me.DataGridView1.Size = New System.Drawing.Size(567, 426)
-        Me.DataGridView1.TabIndex = 2
-        '
-        'RichTextBox1
-        '
-        Me.RichTextBox1.Location = New System.Drawing.Point(35, 280)
-        Me.RichTextBox1.Name = "RichTextBox1"
-        Me.RichTextBox1.Size = New System.Drawing.Size(100, 96)
-        Me.RichTextBox1.TabIndex = 3
-        Me.RichTextBox1.Text = ""
-        '
-        'CheckBox1
-        '
-        Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Location = New System.Drawing.Point(35, 195)
-        Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(124, 19)
-        Me.CheckBox1.TabIndex = 4
-        Me.CheckBox1.Text = "パッシブを出力しない"
-        Me.CheckBox1.UseVisualStyleBackColor = True
-        '
-        'MainWindow
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.CheckBox1)
-        Me.Controls.Add(Me.RichTextBox1)
-        Me.Controls.Add(Me.DataGridView1)
-        Me.Controls.Add(Me.Button1)
-        Me.Name = "MainWindow"
-        Me.Text = "AR2Eチャットパレットジェネレーター"
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.ResumeLayout(False)
-        Me.PerformLayout()
-
+        Button1 = New Button()
+        DataGridView1 = New DataGridView()
+        RichTextBox1 = New RichTextBox()
+        CheckBox1 = New CheckBox()
+        Button2 = New Button()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(75, 73)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(76, 48)
+        Button1.TabIndex = 0
+        Button1.Text = "開く"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(232, 12)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowTemplate.Height = 25
+        DataGridView1.Size = New Size(567, 426)
+        DataGridView1.TabIndex = 2
+        ' 
+        ' RichTextBox1
+        ' 
+        RichTextBox1.Location = New Point(3, 280)
+        RichTextBox1.Name = "RichTextBox1"
+        RichTextBox1.Size = New Size(223, 158)
+        RichTextBox1.TabIndex = 3
+        RichTextBox1.Text = ""' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(12, 255)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(124, 19)
+        CheckBox1.TabIndex = 4
+        CheckBox1.Text = "パッシブを出力しない"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(150, 226)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(76, 48)
+        Button2.TabIndex = 5
+        Button2.Text = "チャパレ出力"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' MainWindow
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button2)
+        Controls.Add(CheckBox1)
+        Controls.Add(RichTextBox1)
+        Controls.Add(DataGridView1)
+        Controls.Add(Button1)
+        Name = "MainWindow"
+        Text = "AR2Eチャットパレットジェネレーター"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
 
     Friend WithEvents Button1 As Button
     Friend WithEvents DataGridView1 As DataGridView
     Friend WithEvents RichTextBox1 As RichTextBox
     Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button2 As Button
 End Class
