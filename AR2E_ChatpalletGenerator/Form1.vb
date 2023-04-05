2693976568b07e06cd33458954baa63ec872353b
musicsoul0142 <mekakushi0142@gmail.com>
2023-04-05 13:13:17 +0900

    チェックボックスを作成

--- AR2E_ChatpalletGenerator/Form1.vb
+++ AR2E_ChatpalletGenerator/Form1.vb
@@ -92,12 +92,12 @@ Public Class MainWindow
         End If
 
         If SkillData("Lv") <> "1" Then
-            SkillChat = $"◆{SkillData("Timing")}◆《{SkillData("Name")}》{{{SkillData("Name")}}}：{Category}/{SkillData("Note")}{Roll}{Target}{Range}{Cost}{Reqd}"
+            SkillChat = $"◆{SkillData("Timing")}◆《{SkillData("Name")}》{{{SkillData("Name")}}}：{Category}{SkillData("Note")}{Roll}{Target}{Range}{Cost}{Reqd}"
         Else
-            SkillChat = $"◆{SkillData("Timing")}◆《{SkillData("Name")}》{SkillData("Lv")}：{Category}/{SkillData("Note")}{Roll}{Target}{Range}{Cost}{Reqd}"
+            SkillChat = $"◆{SkillData("Timing")}◆《{SkillData("Name")}》{SkillData("Lv")}：{Category}{SkillData("Note")}{Roll}{Target}{Range}{Cost}{Reqd}"
         End If
         If SkillData("Cost") <> "―" Then
-            SkillChat = SkillChat & $"{vbCrLf}:MP-{SkillData("Cost")}@{SkillData("Name")}"
+            SkillChat = SkillChat & $"{vbCrLf}:MP-{SkillData("Cost")} @{SkillData("Name")}"
         End If
         If SkillData("Lv") <> "1" Then
             SkillChat = SkillChat & $"{vbCrLf}//{SkillData("Name")}={SkillData("Lv")}"
@@ -132,20 +132,36 @@ Public Class MainWindow
 
             SkillList = Data_Load(loop_type, key_first, key_last_list, default_value, JsonObject)
 
+            RichTextBox1.Text = ""
+
+            For index = 1 To SkillList.Count
+                If (SkillList(index)("Timing") = "パッシブ" Or SkillList(index)("Timing") = "パッシブ／メイキング") And CheckBox1.Checked = True Then
+                    Continue For
+                End If
+                Dim test_text As String = Generate_ChatPallet(SkillList(index))
+                RichTextBox1.AppendText($"{test_text}{vbCrLf}")
+            Next
             For index = 1 To SkillList.Count
                 DataGridView1.Rows.Add()
-                DataGridView1.Rows(index - 1).Cells(0).Value = index
-                DataGridView1.Rows(index - 1).Cells(1).Value = SkillList(index)("Type")
-                DataGridView1.Rows(index - 1).Cells(2).Value = SkillList(index)("Category")
-                DataGridView1.Rows(index - 1).Cells(3).Value = SkillList(index)("Name")
-                DataGridView1.Rows(index - 1).Cells(4).Value = SkillList(index)("Lv")
-                DataGridView1.Rows(index - 1).Cells(5).Value = SkillList(index)("Timing")
-                DataGridView1.Rows(index - 1).Cells(6).Value = SkillList(index)("Note")
-                DataGridView1.Rows(index - 1).Cells(7).Value = SkillList(index)("Roll")
-                DataGridView1.Rows(index - 1).Cells(8).Value = SkillList(index)("Target")
-                DataGridView1.Rows(index - 1).Cells(9).Value = SkillList(index)("Range")
-                DataGridView1.Rows(index - 1).Cells(10).Value = SkillList(index)("Cost")
-                DataGridView1.Rows(index - 1).Cells(11).Value = SkillList(index)("Reqd")
+
+                If (SkillList(index)("Timing") = "パッシブ" Or SkillList(index)("Timing") = "パッシブ／メイキング") And CheckBox1.Checked = True Then
+                    DataGridView1.Rows(index - 1).Cells(0).Value = False
+                Else
+                    DataGridView1.Rows(index - 1).Cells(0).Value = True
+                End If
+
+                DataGridView1.Rows(index - 1).Cells(1).Value = index
+                DataGridView1.Rows(index - 1).Cells(2).Value = SkillList(index)("Type")
+                DataGridView1.Rows(index - 1).Cells(3).Value = SkillList(index)("Category")
+                DataGridView1.Rows(index - 1).Cells(4).Value = SkillList(index)("Name")
+                DataGridView1.Rows(index - 1).Cells(5).Value = SkillList(index)("Lv")
+                DataGridView1.Rows(index - 1).Cells(6).Value = SkillList(index)("Timing")
+                DataGridView1.Rows(index - 1).Cells(7).Value = SkillList(index)("Note")
+                DataGridView1.Rows(index - 1).Cells(8).Value = SkillList(index)("Roll")
+                DataGridView1.Rows(index - 1).Cells(9).Value = SkillList(index)("Target")
+                DataGridView1.Rows(index - 1).Cells(10).Value = SkillList(index)("Range")
+                DataGridView1.Rows(index - 1).Cells(11).Value = SkillList(index)("Cost")
+                DataGridView1.Rows(index - 1).Cells(12).Value = SkillList(index)("Reqd")
 
 
             Next
@@ -157,6 +173,9 @@ Public Class MainWindow
     End Sub
 
     Private Sub MainWindow_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim column As New DataGridViewCheckBoxColumn
+        DataGridView1.Columns.Add(column)
+
         DataGridView1.ColumnCount = 13
         DataGridView1.RowCount = 1
 
@@ -177,16 +196,6 @@ Public Class MainWindow
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Dim SkillList As Object = DataGridView1
-        RichTextBox1.Text = ""
-
-        For index = 1 To SkillList.Count
-            If (SkillList(index)("Timing") = "パッシブ" Or SkillList(index)("Timing") = "パッシブ／メイキング") And CheckBox1.Checked = True Then
-                Continue For
-            End If
-            Dim test_text As String = Generate_ChatPallet(SkillList(index))
-            RichTextBox1.AppendText($"{test_text}{vbCrLf}")
-        Next
 
     End Sub
 End Class
